544672e5652dcf762963ae1d41877bcd2de472f8
buziusbastus1 <kubaotreba121@wp.pl>
2022-05-02 15:22:59 +0200

    initial commit

--- script.js
+++ script.js
@@ -36,254 +36,254 @@ span1.onclick = function () {
 const menu = [
   {
     id: 1,
-    title: 'buttermilk',
+    title: 'pokłon V',
     category: 'gipsoryt',
-    img: './images/rysz/gips/045-pokłonV-min.jpg',
-    anchor: './images/rysz/gips/045-pokłonV-min.jpg'
+    img: './images/rysz/045-poklonV-min.jpg',
+    anchor: './images/rysz/gips/045-poklonV-min.jpg'
   },
   {
     id: 2,
-    title: 'buttermilk',
+    title: 'początek VII',
     category: 'gipsoryt',
-    img: './images/rysz/gips/046początekVII-min.jpg',
-    anchor: './images/rysz/gips/046początekVII-min.jpg'
+    img: './images/rysz/046poczatekVII-min.jpg',
+    anchor: './images/rysz/gips/046poczatekVII-min.jpg'
   },
   {
     id: 3,
-    title: 'buttermilk',
+    title: 'pokłon VII',
     category: 'gipsoryt',
-    img: './images/rysz/gips/051poklonVII-min.jpg',
+    img: './images/rysz/051poklonVII-min.jpg',
     anchor: './images/rysz/gips/051poklonVII-min.jpg'
   },
   {
     id: 4,
-    title: 'buttermilk',
+    title: 'drugie lato Mateusza',
     category: 'gipsoryt',
-    img: './images/rysz/gips/078drugielatoM-min.jpg',
+    img: './images/rysz/078drugielatoM-min.jpg',
     anchor: './images/rysz/gips/078drugielatoM-min.jpg'
   },
   {
     id: 5,
-    title: 'buttermilk',
+    title: 'znalezisko IV',
     category: 'gipsoryt',
-    img: './images/rysz/gips/079n-min.jpg',
+    img: './images/rysz/079n-min.jpg',
     anchor: './images/rysz/gips/079n-min.jpg'
   },
   {
     id: 6,
-    title: 'buttermilk',
+    title: 'znalezisko I',
     category: 'gipsoryt',
-    img: './images/rysz/gips/082n-min.jpg',
+    img: './images/rysz/082n-min.jpg',
     anchor: './images/rysz/gips/082n-min.jpg'
   },
   {
     id: 7,
-    title: 'buttermilk',
+    title: 'zalezisko V',
     category: 'gipsoryt',
-    img: './images/rysz/gips/083zaleziskoVn-min.jpg',
+    img: './images/rysz/083zaleziskoVn-min.jpg',
     anchor: './images/rysz/gips/083zaleziskoVn-min.jpg'
   },
   {
     id: 8,
-    title: 'buttermilk',
+    title: 'zbliżenie',
     category: 'gipsoryt',
-    img: './images/rysz/gips/084zbliżenie-min.jpg',
-    anchor: './images/rysz/gips/084zbliżenie-min.jpg'
+    img: './images/rysz/084zblizenie-min.jpg',
+    anchor: './images/rysz/gips/084zblizenie-min.jpg'
   },
   {
     id: 9,
-    title: 'buttermilk',
+    title: 'cisza',
     category: 'gipsoryt',
-    img: './images/rysz/gips/088ciszaduzy1-min.jpg',
+    img: './images/rysz/088ciszaduzy1-min.jpg',
     anchor: './images/rysz/gips/088ciszaduzy1-min.jpg'
   },
   {
     id: 10,
-    title: 'buttermilk',
+    title: 'skupienie',
     category: 'gipsoryt',
-    img: './images/rysz/gips/089skupienie-min.jpg',
+    img: './images/rysz/089skupienie-min.jpg',
     anchor: './images/rysz/gips/089skupienie-min.jpg'
   },
   {
     id: 11,
-    title: 'buttermilk',
+    title: 'szept szeptów',
     category: 'gipsoryt',
-    img: './images/rysz/gips/093szeptszetow-1-min.jpg',
+    img: './images/rysz/093szeptszetow-1-min.jpg',
     anchor: './images/rysz/gips/093szeptszetow-1-min.jpg'
   },
   {
     id: 12,
-    title: 'buttermilk',
+    title: 'opresja II',
     category: 'gipsoryt',
-    img: './images/rysz/gips/103opresja-min.jpg',
+    img: './images/rysz/103opresja-min.jpg',
     anchor: './images/rysz/gips/103opresja-min.jpg'
   },
   {
     id: 13,
-    title: 'buttermilk',
+    title: 'fragment szeptu',
     category: 'gipsoryt',
-    img: './images/rysz/gips/045-pokłonV-min.jpg',
-    anchor: './images/rysz/gips/045-pokłonV-min.jpg'
+    img: './images/rysz/094-min.jpg',
+    anchor: './images/rysz/gips/094-min.jpg'
   },
   {
     id: 14,
-    title: 'buttermilk',
+    title: 'przedmiot poznania',
     category: 'gipsoryt',
-    img: './images/rysz/gips/124cmykprzedmiotpozn-min.jpg',
-    anchor: './images/rysz/124cmykprzedmiotpozn-min.jpg'
+    img: './images/rysz/124przedmiotpozn.jpg',
+    anchor: './images/rysz/gips/124przedmiotpozn.jpg'
   },
   {
     id: 15,
-    title: 'buttermilk',
+    title: 'rzekome znikanie przedmiotu II',
     category: 'gipsoryt',
-    img: './images/rysz/gips/125rzekZnikPrzedmiotuII-min.jpg',
+    img: './images/rysz/125rzekZnikPrzedmiotuII-min.jpg',
     anchor: './images/rysz/gips/125rzekZnikPrzedmiotuII-min.jpg'
   },
   {
     id: 16,
-    title: 'buttermilk',
+    title: 'wyodrębnienie sygnału VII',
     category: 'gipsoryt',
-    img: './images/rysz/gips/130WyodrSygnałuVI-min.jpg',
-    anchor: './images/rysz/gips/130WyodrSygnałuVI-min.jpg'
+    img: './images/rysz/130WyodrSygnaluVI-min.jpg',
+    anchor: './images/rysz/gips/130WyodrSygnaluVI-min.jpg'
   },
   {
     id: 17,
-    title: 'buttermilk',
+    title: 'dokonywanie wyboru',
     category: 'gipsoryt',
     img: './images/rysz/gips/132ndokonywaniewybory-min.jpg',
     anchor: './images/rysz/gips/132ndokonywaniewybory-min.jpg'
   },
   {
     id: 18,
-    title: 'buttermilk',
+    title: 'pozorna harmonia V',
     category: 'gipsoryt',
-    img: './images/rysz/gips/143pozornharV-min.jpg',
+    img: './images/rysz/143pozornharV-min.jpg',
     anchor: './images/rysz/gips/143pozornharV-min.jpg'
   },
   {
     id: 19,
-    title: 'buttermilk',
+    title: 'pozorna harmonia VII',
     category: 'gipsoryt',
-    img: './images/rysz/gips/145pozornaharmoniaVII-min.jpg',
+    img: './images/rysz/145pozornaharmoniaVII-min.jpg',
     anchor: './images/rysz/gips/145pozornaharmoniaVII-min.jpg'
   },
   {
     id: 20,
-    title: 'buttermilk',
+    title: 'urzeczywistnienie I',
     category: 'gipsoryt',
-    img: './images/rysz/gips/146urzeczywistnienieI-min.jpg',
+    img: './images/rysz/146urzeczywistnienieI-min.jpg',
     anchor: './images/rysz/gips/146urzeczywistnienieI-min.jpg'
   },
   {
     id: 21,
-    title: 'buttermilk',
+    title: 'urzeczywistnienie III',
     category: 'gipsoryt',
-    img: './images/rysz/gips/148nurzeczywistnienieI-min.jpg',
+    img: './images/rysz/148nurzeczywistnienieI-min.jpg',
     anchor: './images/rysz/gips/148nurzeczywistnienieI-min.jpg'
   },
   {
     id: 22,
-    title: 'bison steak',
+    title: 'upamiętnienie I',
     category: 'gipsoryt',
-    img: './images/rysz/gips/149upamiętnienieI-min.jpg',
-    anchor: './images/rysz/gips/149upamiętnienieI-min.jpg'
+    img: './images/rysz/149upamietnienieI-min.jpg',
+    anchor: './images/rysz/gips/149upamietnienieI-min.jpg'
   },
   {
     id: 23,
     title: 'powrót',
     category: 'cyfrowa',
-    img: './images/rysz/fyf/151powrót-min(1).jpg',
-    anchor: './images/rysz/fyf/151powrót-min(1).jpg'
+    img: './images/rysz/151powrot-min(1).jpg',
+    anchor: './images/rysz/fyf/151powrot-min(1).jpg'
   },
   {
     id: 24,
-    title: 'bison steak',
+    title: 'inni',
     category: 'cyfrowa',
-    img: './images/rysz/fyf/179inni-min.jpg',
+    img: './images/rysz/179inni-min.jpg',
     anchor: './images/rysz/fyf/179inni-min.jpg'
   },
   {
     id: 25,
-    title: 'bison steak',
+    title: 'milczenie II',
     category: 'cyfrowa',
-    img: './images/rysz/fyf/221milczenieII-min.jpg',
+    img: './images/rysz/221milczenieII-min.jpg',
     anchor: './images/rysz/fyf/221milczenieII-min.jpg'
   },
   {
     id: 26,
     title: 'milczenie III',
     category: 'cyfrowa',
-    img: './images/rysz/fyf/222milczenieIII-1-min.jpg',
+    img: './images/rysz/222milczenieIII-1-min.jpg',
     anchor: './images/rysz/fyf/222milczenieIII-1-min.jpg'
   },
   {
     id: 27,
-    title: 'bison steak',
+    title: 'szepty IX',
     category: 'cyfrowa',
-    img: './images/rysz/fyf/228szeptyIX-1-min.jpg',
+    img: './images/rysz/228szeptyIX-1-min.jpg',
     anchor: './images/rysz/fyf/228szeptyIX-1-min.jpg'
   },
   {
     id: 28,
-    title: 'bison steak',
+    title: 'intencja II',
     category: 'cyfrowa',
-    img: './images/rysz/fyf/229intencjaII-min.jpg',
+    img: './images/rysz/229intencjaII-min.jpg',
     anchor: './images/rysz/fyf/229intencjaII-min.jpg'
   },
   {
     id: 29,
-    title: 'bison steak',
+    title: 'oswięcimski Dzień Zaduszny II',
     category: 'cyfrowa',
-    img: './images/rysz/fyf/240oswięcimskiDzieńZadusznyII-min.jpg',
-    anchor: './images/rysz/fyf/240oswięcimskiDzieńZadusznyII-min.jpg'
+    img: './images/rysz/240oswiecimskiDzienZadusznyII-min.jpg',
+    anchor: './images/rysz/fyf/240oswiecimskiDzienZadusznyII-min.jpg'
   },
   {
     id: 30,
-    title: 'bison steak',
+    title: 'oswięcimski Dzień Zaduszny III',
     category: 'cyfrowa',
-    img: './images/rysz/fyf/241oswięcimskiDZIIIRGB-min.jpg',
-    anchor: './images/rysz/fyf/241oswięcimskiDZIIIRGB-min.jpg'
+    img: './images/rysz/241oswiecimskiDZIIIRGB-min.jpg',
+    anchor: './images/rysz/fyf/241oswiecimskiDZIIIRGB-min.jpg'
   },
   {
     id: 31,
-    title: 'bison steak',
+    title: 'wyodrębnienie sygnału IX',
     category: 'cyfrowa',
-    img: './images/rysz/fyf/243wyodrSygnałuIX10-min.jpg',
-    anchor: './images/rysz/fyf/243wyodrSygnałuIX10-min.jpg'
+    img: './images/rysz/243wyodrSygnaluIX10-min.jpg',
+    anchor: './images/rysz/fyf/243wyodrSygnaluIX10-min.jpg'
   },
   {
     id: 32,
-    title: 'bison steak',
+    title: 'wyodrębnienie sygnału X',
     category: 'cyfrowa',
-    img: './images/rysz/fyf/244wyodrSygnałuX10-min.jpg',
-    anchor: './images/rysz/fyf/244wyodrSygnałuX10-min.jpg'
+    img: './images/rysz/244wyodrSygnaluX10-min.jpg',
+    anchor: './images/rysz/fyf/244wyodrSygnaluX10-min.jpg'
   },
   {
     id: 33,
-    title: 'bison steak',
+    title: 'wyodrębnienie sygnału XI',
     category: 'cyfrowa',
-    img: './images/rysz/fyf/245wyodrSygnałuXI-min.jpg',
-    anchor: './images/rysz/fyf/245wyodrSygnałuXI-min.jpg'
+    img: './images/rysz/245wyodrSygnaluXI-min.jpg',
+    anchor: './images/rysz/fyf/245wyodrSygnaluXI-min.jpg'
   },
   {
     id: 34,
-    title: 'bison steak',
+    title: 'wspólna pręga XV',
     category: 'cyfrowa',
-    img: './images/rysz/fyf/291wspólnapręgaXV-min.jpg',
-    anchor: './images/rysz/fyf/291wspólnapręgaXV-min.jpg'
+    img: './images/rysz/fyf/291wspolnapregaXV-min.jpg',
+    anchor: './images/rysz/fyf/291wspolnapregaXV-min.jpg'
   },
   {
     id: 35,
-    title: 'bison steak',
+    title: 'oczyszczenie II',
     category: 'cyfrowa',
-    img: './images/rysz/fyf/361oczyszczenieII-1-min.jpg',
+    img: './images/rysz/361oczyszczenieII-1-min.jpg',
     anchor: './images/rysz/fyf/361oczyszczenieII-1-min.jpg'
   },
   {
     id: 36,
-    title: 'bison steak',
+    title: 'oczyszczenie III',
     category: 'cyfrowa',
-    img: './images/rysz/fyf/362oczyszczenieIII-1-min.jpg',
+    img: './images/rysz/362oczyszczenieIII-1-min.jpg',
     anchor: './images/rysz/fyf/362oczyszczenieIII-1-min.jpg'
   }
 ]
@@ -298,11 +298,9 @@ window.addEventListener('DOMContentLoaded', function () {
 
 function diplayMenuItems (menuItems) {
   let displayMenu = menuItems.map(function (item) {
-    // console.log(item);
-
     return `<article class="menu-item">
     <a data-pswp-src=${item.anchor} data-pswp-width="1500"  data-pswp-height="2100">
-          <img src=${item.img} alt="${item.title}" class="photo "  />
+          <img loading="auto" src=${item.img} alt="${item.title}" class="photo "  />
           </a>
           <div class="item-info">
             <header>
@@ -364,162 +362,162 @@ const menu1 = [
     id: 1,
     title: 'sennik pomarańcze',
     category1: 'cyfrowa',
-    img: './images/jad/cyf/38_sennikipomarancze.jpg',
+    img: './images/jad/38_sennikipomarancze.jpg',
     anchor: './images/jad/cyf/38_sennikipomarancze.jpg'
   },
   {
     id: 2,
-    title: 'diner doublee',
+    title: 'sennik i martwa natura',
     category1: 'cyfrowa',
-    img: './images/jad/cyf/39_sennikimartwanatura.jpg',
+    img: './images/jad/39_sennikimartwanatura.jpg',
     anchor: './images/jad/cyf/39_sennikimartwanatura.jpg'
   },
   {
     id: 3,
-    title: 'godzilla milkshake',
+    title: 'klinika III',
     category1: 'cyfrowa',
-    img: './images/jad/cyf/40_klinikaIII-1.jpg',
+    img: './images/jad/40_klinikaIII-1.jpg',
     anchor: './images/jad/cyf/40_klinikaIII-1.jpg'
   },
   {
     id: 4,
-    title: 'country delight',
+    title: 'wesele III',
     category1: 'cyfrowa',
-    img: './images/jad/cyf/41_weseleIII.jpg',
+    img: './images/jad/41_weseleIII.jpg',
     anchor: './images/jad/cyf/41_weseleIII.jpg'
   },
   {
     id: 5,
-    title: 'egg attack',
+    title: 'dramatczna',
     category1: 'drzeworyt',
-    img: './images/jad/drzewor/09-dramatczna.jpg',
+    img: './images/jad/09-dramatczna.jpg',
     anchor: './images/jad/drzewor/09-dramatczna.jpg'
   },
   {
     id: 6,
-    title: 'oreo dream',
+    title: 'przechodnie II',
     category1: 'drzeworyt',
-    img: './images/jad/drzewor/13przechodnieII.jpg',
+    img: './images/jad/13przechodnieII.jpg',
     anchor: './images/jad/drzewor/13przechodnieII.jpg'
   },
   {
     id: 7,
-    title: 'bacon overflow',
+    title: 'czarne i biale zamknięcia',
     category1: 'drzeworyt',
-    img: './images/jad/drzewor/14czarneibiałezamknięia.jpg',
-    anchor: './images/jad/drzewor/14czarneibiałezamknięia.jpg'
+    img: './images/jad/14czarneibialezamknieia.jpg',
+    anchor: './images/jad/drzewor/14czarneibialezamknieia.jpg'
   },
   {
     id: 8,
-    title: 'american classic',
+    title: 'gra',
     category1: 'drzeworyt',
-    img: './images/jad/drzewor/20-gra.jpg',
+    img: './images/jad/20-gra.jpg',
     anchor: './images/jad/drzewor/20-gra.jpg'
   },
   {
     id: 9,
-    title: 'quarantine buddy',
+    title: 'akt',
     category1: 'drzeworyt',
-    img: './images/jad/drzewor/21-aktcol.jpg',
+    img: './images/jad/21-aktcol.jpg',
     anchor: './images/jad/drzewor/21-aktcol.jpg'
   },
   {
     id: 10,
-    title: 'bison steak',
+    title: 'sennik i kreska',
     category1: 'drzeworyt',
-    img: './images/jad/drzewor/26-sennikIkreska.jpg',
+    img: './images/jad/26-sennikIkreska.jpg',
     anchor: './images/jad/drzewor/26-sennikIkreska.jpg'
   },
   {
     id: 11,
-    title: 'bison steak',
+    title: 'klinika II',
     category1: 'gipsoryt',
-    img: './images/jad/gipsor/30-klinikaII68.jpg',
+    img: './images/jad/30-klinikaII68.jpg',
     anchor: './images/jad/gipsor/30-klinikaII68.jpg'
   },
   {
     id: 12,
-    title: 'bison steak',
+    title: 'odejścia III',
     category1: 'gipsoryt',
-    img: './images/jad/gipsor/31-odejściaIII.jpg',
-    anchor: './images/jad/gipsor/31-odejściaIII.jpg'
+    img: './images/jad/31-odejsciaIII.jpg',
+    anchor: './images/jad/gipsor/31-odejsciaIII.jpg'
   },
   {
     id: 13,
-    title: 'bison steak',
+    title: 'bez powrotu',
     category1: 'gipsoryt',
-    img: './images/jad/gipsor/32gonebeyongreturn.jpg',
-    anchor: './images/jad/gipsor/32gonebeyongreturn.jpg'
+    img: './images/jad/32-gone.jpg',
+    anchor: './images/jad/gipsor/32-gone.jpg'
   },
   {
     id: 14,
-    title: 'bison steak',
+    title: 'klinika IV',
     category1: 'gipsoryt',
-    img: './images/jad/gipsor/33-klinikaIV.jpg',
+    img: './images/jad/33-klinikaIV.jpg',
     anchor: './images/jad/gipsor/33-klinikaIV.jpg'
   },
   {
     id: 15,
-    title: 'bison steak',
+    title: 'lewiatan',
     category1: 'gipsoryt',
-    img: './images/jad/gipsor/35-Lewiatangrey.jpg',
+    img: './images/jad/35-Lewiatangrey.jpg',
     anchor: './images/jad/gipsor/35-Lewiatangrey.jpg'
   },
   {
     id: 16,
-    title: 'bison steak',
+    title: 'lunapark',
     category1: 'gipsoryt',
-    img: './images/jad/gipsor/36-Lunapark.jpg',
+    img: './images/jad/36-Lunapark.jpg',
     anchor: './images/jad/gipsor/36-Lunapark.jpg'
   },
   {
     id: 17,
-    title: 'bison steak',
-    category1: 'gipsoryt',
-    img: './images/jad/linor/01-groteska.jpg',
+    title: 'groteska',
+    category1: 'linoryt',
+    img: './images/jad/01-groteska.jpg',
     anchor: './images/jad/linor/01-groteska.jpg'
   },
   {
     id: 18,
-    title: 'bison steak',
-    category1: 'gipsoryt',
-    img: './images/jad/linor/15-żałoba64.jpg',
-    anchor: './images/jad/linor/15-żałoba64.jpg'
+    title: 'żaloba',
+    category1: 'linoryt',
+    img: './images/jad/15-zaloba64.jpg',
+    anchor: './images/jad/linor/15-zaloba64.jpg'
   },
   {
     id: 19,
-    title: 'bison steak',
-    category1: 'gipsoryt',
-    img: './images/jad/linor/22-mocny.jpg',
+    title: 'mocny',
+    category1: 'linoryt',
+    img: './images/jad/22-mocny.jpg',
     anchor: './images/jad/linor/22-mocny.jpg'
   },
   {
     id: 20,
-    title: 'bison steak',
-    category1: 'gipsoryt',
+    title: 'wesele',
+    category1: 'linoryt',
     img: './images/jad/linor/24-wesele.jpg',
     anchor: './images/jad/linor/24-wesele.jpg'
   },
   {
     id: 21,
-    title: 'bison steak',
-    category1: 'gipsoryt',
-    img: './images/jad/linor/28_klinikab&w.jpg',
+    title: 'klinika',
+    category1: 'linoryt',
+    img: './images/jad/28_klinikab&w.jpg',
     anchor: './images/jad/linor/28_klinikab&w.jpg'
   },
   {
     id: 22,
-    title: 'bison steak',
-    category1: 'gipsoryt',
-    img: './images/jad/linor/34-Morfeusz.jpg',
+    title: 'Morfeusz',
+    category1: 'linoryt',
+    img: './images/jad/34-Morfeusz.jpg',
     anchor: './images/jad/linor/34-Morfeusz.jpg'
   },
   {
     id: 23,
-    title: 'bison steak',
-    category1: 'gipsoryt',
-    img: './images/jad/linor/odejścieII.jpg',
-    anchor: './images/jad/linor/odejścieII.jpg'
+    title: 'odejście II',
+    category1: 'linoryt',
+    img: './images/jad/odejscieII.jpg',
+    anchor: './images/jad/linor/odejscieII.jpg'
   }
 ]
 // get parent element
@@ -537,7 +535,7 @@ function diplayMenuItems1 (menuItems1) {
 
     return `<article class="menu-item">
     <a data-pswp-src=${item.anchor} data-pswp-width="1500"  data-pswp-height="2100">
-          <img src=${item.img} alt="${item.title}" class="photo "  />
+          <img loading="auto" src=${item.img} alt="${item.title}" class="photo "  />
           </a>
           <div class="item-info">
             <header>
